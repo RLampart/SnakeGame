23fa96fb7521458b1f3929d6f4291e9f0ccd3586
RLampart <rockingriches@gmail.com>
2024-07-24 18:01:17 -0500

    Add project files.

---
+++ SnakeGame/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Timer timer1;
+
+
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 100;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            this.ResumeLayout(false);
+        }
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        //#region Windows Form Designer generated code
+
+        ///// <summary>
+        ///// Required method for Designer support - do not modify
+        ///// the contents of this method with the code editor.
+        ///// </summary>
+        //private void InitializeComponent()
+        //{
+        //    this.components = new System.ComponentModel.Container();
+        //    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        //    this.ClientSize = new System.Drawing.Size(800, 450);
+        //    this.Text = "Form1";
+        //}
+
+        //#endregion
+    }
+}
+
---
+++ SnakeGame/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> snake = new List<Point>();
+        private Point food;
+        private Direction currentDirection = Direction.Right;
+        private Random random = new Random();
+        private int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            snake.Clear();
+            snake.Add(new Point(5, 5));
+            GenerateFood();
+            timer1.Start();
+            currentDirection = Direction.Right;
+            this.KeyDown += new KeyEventHandler(OnKeyDown);
+        }
+
+        private void GenerateFood()
+        {
+            int maxX = this.ClientSize.Width / 20;
+            int maxY = this.ClientSize.Height / 20;
+            food = new Point(random.Next(0, maxX), random.Next(0, maxY));
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    if (currentDirection != Direction.Down)
+                        currentDirection = Direction.Up;
+                    break;
+                case Keys.Down:
+                    if (currentDirection != Direction.Up)
+                        currentDirection = Direction.Down;
+                    break;
+                case Keys.Left:
+                    if (currentDirection != Direction.Right)
+                        currentDirection = Direction.Left;
+                    break;
+                case Keys.Right:
+                    if (currentDirection != Direction.Left)
+                        currentDirection = Direction.Right;
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoveSnake();
+            CheckCollision();
+            this.Invalidate();
+        }
+
+        private void MoveSnake()
+        {
+            Point head = snake[0];
+            Point newHead = head;
+
+            switch (currentDirection)
+            {
+                case Direction.Up:
+                    newHead.Y--;
+                    break;
+                case Direction.Down:
+                    newHead.Y++;
+                    break;
+                case Direction.Left:
+                    newHead.X--;
+                    break;
+                case Direction.Right:
+                    newHead.X++;
+                    break;
+            }
+
+            snake.Insert(0, newHead);
+            if (newHead == food)
+            {
+                score++;
+                GenerateFood();
+            }
+            else
+            {
+                snake.RemoveAt(snake.Count - 1);
+            }
+        }
+
+        private void GhostSnake(char wall)
+        {
+            Point head = snake[0];
+            Point newHead = head;
+
+            switch (wall)
+            {
+                case 'U':
+                    newHead.Y = this.ClientSize.Height / 20;
+                    break;
+                case 'D':
+                    newHead.Y = 0;
+                    break;
+                case 'L':
+                    newHead.X = this.ClientSize.Width / 20;
+                    break;
+                case 'R':
+                    newHead.X = 0;
+                    break;
+            }
+
+            snake.Insert(0, newHead);
+            if (newHead == food)
+            {
+                score++;
+                GenerateFood();
+            }
+            else
+            {
+                snake.RemoveAt(snake.Count - 1);
+            }
+        }
+
+        private void CheckCollision()
+        {
+            Point head = snake[0];
+
+            // Check wall collision
+            if (head.X < 0 )
+            {
+               GhostSnake('L'); 
+            }else if (head.X >= this.ClientSize.Width / 20) { GhostSnake('R'); }
+            else if (head.Y < 0) { GhostSnake('U'); }
+            else if (head.Y >= this.ClientSize.Height / 20) { GhostSnake('D'); }
+
+            head = snake[0];
+            // Check self collision
+            for (int i = 1; i < snake.Count; i++)
+            {
+                if (snake[i] == head)
+                {
+                    GameOver();
+                }
+            }
+        }
+
+        private void GameOver()
+        {
+            timer1.Stop();
+            MessageBox.Show("Game Over! Your score: " + score);
+            InitializeGame();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Draw snake
+            foreach (Point point in snake)
+            {
+                e.Graphics.FillEllipse(Brushes.Black, new Rectangle(point.X * 20, point.Y * 20, 20, 20));
+            }
+
+            // Draw food
+            e.Graphics.FillEllipse(Brushes.Red, new Rectangle(food.X * 20, food.Y * 20, 20, 20));
+        }
+
+        private enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
+    }
+}
